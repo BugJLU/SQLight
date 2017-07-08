39d994982d6bee0ecdf7239829eb2aa46eb76138
HanYec <i@qcblog.cn>
2017-07-08 22:46:49 +0800

    Add KeyboardListener

--- app/src/main/java/org/bugjlu/sqlight/DBExecActivity.java
+++ app/src/main/java/org/bugjlu/sqlight/DBExecActivity.java
@@ -2,6 +2,7 @@ package org.bugjlu.sqlight;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
@@ -10,20 +11,13 @@ import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ListView;
 import android.widget.ScrollView;
-import android.widget.SimpleAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.bugjlu.sqlight.dbexec_handlers.IDBExecHandler;
 import org.bugjlu.sqlight.dbexec_handlers.SQLiteHandler;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 public class DBExecActivity extends AppCompatActivity {
 
@@ -59,20 +53,38 @@ public class DBExecActivity extends AppCompatActivity {
 //        resultView.setAdapter(resultView.getAdapter());
         //TODO: test end
 
-        stmtText.setOnKeyListener(new View.OnKeyListener() {
+//        stmtText.setOnKeyListener(new View.OnKeyListener() {
+//            @Override
+//            public boolean onKey(View v, int keyCode, KeyEvent event) {
+//                if ((event.getAction() == KeyEvent.ACTION_UP) && (keyCode == KeyEvent.KEYCODE_BACK)) {
+//                    stmtText.clearFocus();
+//                    return true;
+//                }
+//                return false;
+//            }
+//        });
+
+//        stmtText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+//            @Override
+//            public void onFocusChange(View v, boolean hasFocus) {
+//                if (hasFocus) {
+//                    Animation animation = new AlphaAnimation(1.0f, 0.0f);
+//                    animation.setDuration(100);
+//                    resultView.setAnimation(animation);
+//                    resultView.setVisibility(View.GONE);
+//                } else {
+//                    resultView.setVisibility(View.VISIBLE);
+//                    Animation animation = new AlphaAnimation(0.0f, 1.0f);
+//                    animation.setDuration(100);
+//                    resultView.setAnimation(animation);
+//                }
+//            }
+//        });
+
+        new KeyboardChangeListener(this).setKeyBoardListener(new KeyboardChangeListener.KeyBoardListener() {
             @Override
-            public boolean onKey(View v, int keyCode, KeyEvent event) {
-                if ((event.getAction() == KeyEvent.ACTION_UP) && (keyCode == KeyEvent.KEYCODE_BACK)) {
-                    stmtText.clearFocus();
-                    return true;
-                }
-                return false;
-            }
-        });
-        stmtText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
-            @Override
-            public void onFocusChange(View v, boolean hasFocus) {
-                if (hasFocus) {
+            public void onKeyboardChange(boolean isShow, int keyboardHeight) {
+                if(isShow) {
                     Animation animation = new AlphaAnimation(1.0f, 0.0f);
                     animation.setDuration(100);
                     resultView.setAnimation(animation);
@@ -85,6 +97,7 @@ public class DBExecActivity extends AppCompatActivity {
                 }
             }
         });
+
         stmtText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
@@ -95,18 +108,34 @@ public class DBExecActivity extends AppCompatActivity {
                 return false;
             }
         });
-        ((Button) findViewById(R.id.execButton)).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.execButton).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 execDB();
             }
         });
+
         startIntent = getIntent();
         Bundle bundle = startIntent.getExtras();
         dbname = bundle.getString("title");
         dbfile = bundle.getString("info");
         connectDB(dbfile);
         prefix = execHandler.getDBProduct() + "$ ";
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+        if (newConfig.hardKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_NO) {
+            Toast.makeText(this, "keyboard visible", Toast.LENGTH_SHORT).show();
+            resultView.setVisibility(View.GONE);
+        } else if (newConfig.hardKeyboardHidden == Configuration.HARDKEYBOARDHIDDEN_YES) {
+            Toast.makeText(this, "keyboard hidden", Toast.LENGTH_SHORT).show();
+            resultView.setVisibility(View.VISIBLE);
+        }
+
     }
 
     @Override
@@ -118,9 +147,11 @@ public class DBExecActivity extends AppCompatActivity {
     protected void connectDB(String dbfile) {
         execHandler = SQLiteHandler.openSQLiteDB(this, dbfile);
     }
+
     protected void disconnectDB() {
         execHandler.close();
     }
+
     protected void execDB() {
         String stmt = stmtText.getText().toString();
         stmt = stmt.replace("\n", " ");
@@ -130,7 +161,7 @@ public class DBExecActivity extends AppCompatActivity {
                 .getSystemService(Context.INPUT_METHOD_SERVICE)).
                 hideSoftInputFromWindow(stmtText.getWindowToken(), 0);
         resultText.append(prefix + stmt + "\n");
-        resultText.append(result+"\n");
+        resultText.append(result + "\n");
         //TODO: test start
 //        HashMap m = new HashMap<String, String>();
 //        m.put("title", result);
